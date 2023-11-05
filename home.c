5d702e7c6ef4352e8b206618aeb0b27a2c94a432
Imadnajam <imad00naj11@gmail.com>
2023-11-05 20:21:23 +0100

    test git day 21

--- home.c
+++ home.c
@@ -1,4 +1,38 @@
-Hello
-Imad
-Abderrahim
-Bonjour
+
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("Welcome to My Homepage\n");
+        printf("1. About\n");
+        printf("2. Portfolio\n");
+        printf("3. Contact\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("About Me:\n");
+                printf("I am a C programmer. This is my simple homepage.\n");
+                break;
+            case 2:
+                printf("My Portfolio:\n");
+                printf("I don't have a portfolio yet.\n");
+                break;
+            case 3:
+                printf("Contact Me:\n");
+                printf("You can reach me at myemail@example.com\n");
+                break;
+            case 4:
+                printf("Goodbye!\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
+}
---
+++ home.ru
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AboutButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        // Implement behavior for About button click here
+        Debug.Log("About button clicked");
+    }
+}
